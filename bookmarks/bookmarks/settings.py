2e88e9fb5e695dc79e3eea1f76204539f071c6b7
Romas <Romashka951@gmail.com>
2020-02-20 16:40:34 +0500

    Added Google authentication

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -113,6 +113,7 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
     'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.google.GoogleOAuth2',
 ]
 
 # Internationalization
